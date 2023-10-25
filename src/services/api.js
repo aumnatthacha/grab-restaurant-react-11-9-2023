cb2ef8ae4dea8cb9362084a110068c15ac729426
aumnatthacha <aummumdaeng@gmail.com>
2023-10-25 16:47:25 +0700

    add,api

---
+++ src/services/api.js
@@ -0,0 +1,59 @@
+//22
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const USERNAME = import.meta.env.VITE_BASE_USERNAME;
+const PASSWORD = import.meta.env.VITE_BASE_PASSWORD;
+import TokenService from "./token-services";
+
+const instance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    auth: {
+        username: USERNAME,
+        password: PASSWORD
+    }
+});
+
+//Add interceptor to request object
+instance.interceptors.request.use((config) => {
+    const token = TokenService.getLocalAccessToken();
+    if (token) {
+        config.headers["X-access-token"] = token;
+    }
+    return config;
+},
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+//Add interceptor to reponse object
+instance.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    async (err) => {
+        const originalConfig = err.config;
+        if (originalConfig.url !== "/api/auth/signin" && err.response) {
+            if (err.response.status === 401 && !originalConfig._retry) {
+                originalConfig._retry = true;
+                try {
+                    const rs = await instance.post("/api/auth/refreshToken", {
+                        refreshToken: TokenService.getLocalRefreshToken()
+                    })
+                    const { accessToken } = rs.date;
+                    TokenService.setLocalAccessToken(accessToken);
+                    return instance(originalConfig)
+                }
+                catch (_error) {
+                    return Promise.reject(_error);
+                }
+            }
+        }
+        return Promise.reject(err)
+    }
+);
+export default instance;
